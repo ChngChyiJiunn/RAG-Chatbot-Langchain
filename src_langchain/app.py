f8512dfd85d43ffbf37f709050c84676e2ddf7ab
Chng Chyi Jiunn <cj.chng10@gmail.com>
2024-08-26 00:55:09 +0100

    add src files

---
+++ src_langchain/app.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import os
+from model import LLM
+from rag_utils import EmbeddingModel, ChromaDB, load_and_split
+import hashlib
+import torch
+import time
+
+
+FILES_DIR = os.path.normpath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "files")
+)
+
+st.set_page_config(page_title="Streamlit Chatbot", page_icon="🤖")
+st.title("Otter")
+
+@st.cache_resource
+def load_model():
+    model = LLM(model_id="microsoft/phi-2",trust_remote_code=True,torch_dtype= torch.bfloat16, device_map = "cuda")
+    return model
+
+
+@st.cache_resource
+def load_embedding_model():
+    embedding_model = EmbeddingModel(embed_id="BAAI/bge-small-en-v1.5", device="cpu")
+    return embedding_model
+
+# Set to store hashes of documents
+@st.cache_resource
+def create_document_set():
+    document_hashes = set()
+    return document_hashes
+
+@st.cache_resource
+def load_db():
+    db = ChromaDB(embedding_model.embed_model)
+    return db
+
+model = load_model()
+embedding_model = load_embedding_model()
+document_hashes = create_document_set()
+
+
+
+def save_file(uploaded_file):
+
+    os.makedirs(FILES_DIR, exist_ok=True)
+    file_path = os.path.join(FILES_DIR, uploaded_file.name)
+    
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return file_path
+
+
+
+
+def add_document(docs):
+    """
+    Check single documents name
+    """
+    
+    # Compute SHA256 hash of the document
+    for doc in docs:
+        doc_hash = hashlib.sha256(doc.name.encode()).hexdigest()
+    
+    # Check if hash is in the set
+    if doc_hash in document_hashes:
+        #print("Duplicate document detected!")
+        return False
+    else:
+        document_hashes.add(doc_hash)
+        #print("Document added successfully!")
+        return True
+
+
+
+def main():        
+    with st.sidebar:
+        max_new_tokens = st.number_input("max_new_tokens",128,4096,512)
+        k = st.number_input("k",1,10,2)
+        temperature = st.number_input("temperature",0.1,1.0,0.25)
+        uploaded_files = st.file_uploader("Upload a .pdf File",type=["PDF","pdf"],accept_multiple_files=True)
+        # if there is any uploaded file, save documents to db
+        if uploaded_files != []:
+            db = load_db()
+            #db = ChromaDB(embedding_model.embed_model)
+            if add_document(uploaded_files):
+                file_paths = [save_file(uploaded_file) for uploaded_file in uploaded_files]
+                docs = load_and_split(file_paths)
+                db.save(docs)
+                
+            
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("How can I help you human today?"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        
+
+        # Display LLM assistant reponse in chat message container
+        with st.chat_message("assistant"):
+            context = (None if uploaded_files == [] else db.retrieve(prompt,
+                                                                    k=k))
+            
+            #response = model.generate(prompt,context,max_new_tokens=max_new_tokens,temperature=temperature)
+            #response = st.write_stream(model.stream_response(prompt,context,max_new_tokens=max_new_tokens,temperature=temperature))
+
+            response_container = st.empty()  # Create an empty container for the streaming response
+            full_response = ""  # Accumulate the full response here
+            response = model.chain_response(prompt, context, max_new_tokens=max_new_tokens, temperature=temperature)
+            for chunk in response.replace("$","\$") :
+                full_response += chunk  # Accumulate chunks into the full response
+                time.sleep(0.01)
+                response_container.markdown(full_response + "▌")  # Display current progress with a cursor
+
+            # Final update to remove the cursor
+            response_container.markdown(full_response)
+
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+        
+
+if __name__ == "__main__":
+    main()
---
+++ src_langchain/model.py
@@ -0,0 +1,137 @@
+from transformers import AutoModelForCausalLM,AutoTokenizer,BitsAndBytesConfig,GenerationConfig, pipeline
+from langchain_huggingface.llms import HuggingFacePipeline
+import torch
+import os
+from langchain.schema.runnable import RunnablePassthrough,RunnableParallel
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+import time
+
+CACHE_DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","models"))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class LLM:
+    def __init__(self,model_id: str = "microsoft/phi-2",trust_remote_code:bool= True,**kwargs) -> None:
+        
+        compute_dtype = getattr(torch,"bfloat16")
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit= True,
+            bnb_4bit_quant_type= "nf4",
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype= compute_dtype,
+            )
+
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id,cache_dir=CACHE_DIR)
+        self.model = AutoModelForCausalLM.from_pretrained(model_id,trust_remote_code=trust_remote_code,cache_dir=CACHE_DIR,**kwargs)
+        self.device = device
+
+
+    def get_pipeline(self, temperature:float=0, do_sample:bool = True, repetition_penalty:float=None,**kwargs):
+        gen_pipeline = pipeline(task="text-generation",
+                                model=self.model,
+                                tokenizer=self.tokenizer,
+                                temperature=temperature,
+                                do_sample = do_sample,
+                                repetition_penalty=repetition_penalty,
+                                #device =self.device,
+                                pad_token_id = self.tokenizer.eos_token_id,
+                                **kwargs)
+        llm = HuggingFacePipeline(pipeline=gen_pipeline)
+        return llm
+        
+
+
+    def generate(self,question:str, context:str = None, max_new_tokens:int = 128,temperature:float=0.25):
+        if context == None or context=="":
+            chat =  f"Instruct:{question}\nOutput:" 
+        else:
+            chat =  f"Instruct: Use the following context to generate a response. Context: {context} {question}\nOutput:"               
+           
+        # if context == None or context=="": # If cannot find context
+        #     chat =[
+        #     {"role":"assistant",
+        #      "content":"""You are a friendly chatbot assistant that gives structured output.
+        #     """
+        #     },
+        #     {"role":"user",
+        #      "content":f"Questions:{question}"}
+        #     ]
+        # else:
+        #     chat =[
+        #         {"role":"assistant",
+        #         "content":"""You are a friendly chatbot assistant that gives structured output.
+        #         Instructions: Use the context to help you to generate the output. 
+        #         """
+        #         },
+        #         {"role":"user",
+        #         "content":f"Here is the context:{context}. Questions:{question}"}
+        #     ]
+
+
+        # if context == None or context=="": # If cannot find context
+        #     chat =[
+        #     {"role":"user",
+        #      "content":f"Questions: {question}"}
+        #     ]
+        # else:
+ 
+        #     chat =[
+        #         {"role":"user",
+        #         "content":f"Here is the context: {context}. Questions: {question}"}
+        #     ]
+        # tokenized_chat = self.tokenizer.apply_chat_template(chat,
+        #                                                     tokenize=True,
+        #                                                     add_generation_prompt=False,
+        #                                                     #skip_special_tokens =True,
+        #                                                     return_attention_mask= False,
+        #                                                     return_tensors="pt").to(self.device)
+        generation_config = GenerationConfig(max_new_tokens = max_new_tokens,
+                                            temperature = temperature,
+                                            do_sample = True,
+                                            pad_token_id = self.tokenizer.eos_token_id
+                                            )
+        
+        tokenized_chat = self.tokenizer(
+            chat, add_special_tokens=False, return_tensors="pt"
+        ).to(self.device)
+        with torch.no_grad():
+            outputs = self.model.generate(**tokenized_chat,
+                                          generation_config=generation_config)
+        response = self.tokenizer.decode(outputs[0][len(tokenized_chat['input_ids'][0]):],skip_special_tokens=True)
+        
+
+        return response
+
+
+    def chain_response(self,question:str,context:str=None, max_new_tokens:int = 128,temperature:float=0.25):
+        if context == None or context=="":
+            prompt_template = """Instruct:{question}\nOutput:"""
+            chat =  PromptTemplate(input_variables=["question"],template=prompt_template)
+        else:
+            prompt_template = """Instruct: Use the following context to generate a response. Context: {context} {question}\nOutput:""" 
+            chat =  PromptTemplate(input_variables=["context","question"],template=prompt_template)
+        
+        llm = self.get_pipeline(max_new_tokens=max_new_tokens,temperature=temperature,return_full_text= False)
+        input_variables = {"context":context,"question":question}
+
+        rag_chain = (RunnablePassthrough()
+                    | chat
+                    | llm
+                    | StrOutputParser()
+                    )
+                    #| (lambda x: x.replace("$","\$"))
+                    
+        # for chunk in rag_chain.stream(input_variables):
+        #     time.sleep(1)
+        #     yield chunk
+               
+
+
+        return rag_chain.invoke(input_variables)
+
+
+
+
+        
